40070046b2fc1be117340c4f1e16c5d3d0666366
Tiina Malinen <malitsu@gmail.com>
2019-07-03 10:58:12 +0300

    Execise 2.10

--- osa2/puhelinluettelo/src/index.js
+++ osa2/puhelinluettelo/src/index.js
@@ -7,6 +7,87 @@ const Person = ({person}) => {
     )
 }
 
+const Persons = ({persons, search}) => {
+
+  const personsToShow = (search === '')
+  ? persons
+  : persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase()))
+
+  const rows = () => personsToShow.map(person =>
+      <Person
+          key={person.name}
+          person={person}
+      />
+  )
+
+  return (
+    <div>
+      {rows()}
+    </div>
+  )
+}
+
+const Filter = ({search, setSearch}) => {
+    const handleSearchChange = (event) => {
+      setSearch(event.target.value)
+    }
+
+    return (
+      <form>
+          filter shown with:    <input
+                                    value={search}
+                                    onChange={handleSearchChange}    
+                                />
+      </form>
+    )
+}
+
+const PersonForm = ({persons, newName, newNumber, setNewName, setNewNumber, setPersons}) => {
+    const handleNameChange = (event) => {
+        setNewName(event.target.value)
+    }
+
+    const handleNumberChange = (event) => {
+        setNewNumber(event.target.value)
+    }
+
+    const addPerson= (event) => {
+        event.preventDefault()
+        const personObject = {
+          name: newName,
+          number: newNumber,
+          date: new Date().toISOString(),
+          important: Math.random() > 0.5
+        }
+        setNewName('')
+        setNewNumber('')
+        if (persons.filter(person => person.name === newName).length > 0){
+            window.alert(`${newName} is already added to phonebook`)
+        }
+        else {
+          setPersons(persons.concat(personObject))
+        }
+    }
+
+    return (
+      <form onSubmit={addPerson}>
+      <div>
+        name: <input
+                  value={newName}
+                  onChange={handleNameChange}    
+              />
+      </div>
+      <div>
+          number: <input 
+                      value={newNumber}
+                      onChange={handleNumberChange}
+                  />
+      </div>
+        <button type="submit">add</button>
+    </form>
+    )
+}
+
 const App = () => {
   const [ persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456' },
@@ -19,80 +100,27 @@ const App = () => {
   const [ newNumber, setNewNumber ] = useState('')
   const [ search, setSearch ] = useState('')
 
-  const addPerson= (event) => {
-      event.preventDefault()
-      const personObject = {
-        name: newName,
-        number: newNumber,
-        date: new Date().toISOString(),
-        important: Math.random() > 0.5
-      }
-      setNewName('')
-      setNewNumber('')
-      if (persons.filter(person => person.name === newName).length > 0){
-          window.alert(`${newName} is already added to phonebook`)
-      }
-      else {
-        setPersons(persons.concat(personObject))
-      }
-  }
-  
-  const handleNameChange = (event) => {
-      setNewName(event.target.value)
-  }
-
-  const handleNumberChange = (event) => {
-      setNewNumber(event.target.value)
-  }
-
-  const handleSearchChange = (event) => {
-      setSearch(event.target.value)
-  }
-
-  const personsToShow = (search === '')
-    ? persons
-    : persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase()))
-
-  const rows = () => personsToShow.map(person =>
-        <Person
-            key={person.name}
-            person={person}
-        />
-    )
-
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-          <div>
-              filter shown with:    <input
-                                        value={search}
-                                        onChange={handleSearchChange}    
-                                    />
-          </div>
-      </form>
+      <Filter
+          search={search}
+          setSearch={setSearch}
+      />
       <h2>add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input
-                    value={newName}
-                    onChange={handleNameChange}    
-                />
-        </div>
-        <div>
-            number: <input 
-                        value={newNumber}
-                        onChange={handleNumberChange}
-                    />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm
+          persons={persons}
+          newName={newName}
+          newNumber={newNumber}
+          setNewName={setNewName}
+          setNewNumber={setNewNumber}
+          setPersons={setPersons}
+      />
       <h2>Numbers</h2>
-        <div>
-            {rows()}
-        </div>
+        <Persons 
+          persons={persons}
+          search={search} 
+        />
     </div>
   )
 
